cc7c11e90b4669b0618efc0a58c3587ac924ddb6
ahmedsamir636869 <www.cood70da@gmail.com>
2024-05-09 18:45:15 +0300

    Changes

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,45 +4,32 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcryptjs'
 
-export const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {},
-            
-            async authorize(credentials) {
-                const {email, password} = credentials;
-                
-                try {
-                    await connectMongoDB();
-                    const user = await User.findOne({ email });
+export default async (req, res) => {
+    if (req.method === 'POST') {
+        const { email, password } = req.body;
 
-                    if (!user) return null;
-                    
-                
-                    const passwordsMatch = await bcrypt.compare(password, user.password);
+        try {
+            await connectMongoDB();
+            const user = await User.findOne({ email });
 
-                    if (!passwordsMatch) return null;
-                    
-                    console.log('User with ID: ', user._id, ' and Email of: ', user.email, ' has logged in successfully!')
-
-                    return user;
-                } catch (error) {
-                    console.log('Login Error: ', error)
-                }
-
+            if (!user) {
+                return res.status(401).json({ error: 'Invalid email or password' });
             }
-        }),
-    ],
-    session: {
-        strategy: 'jwt',
-    },
-    secret: process.env.NEXTAUTH_SECRET, 
-    pages: {
-        signIn: '/login',
+            
+            const passwordsMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordsMatch) {
+                return res.status(401).json({ error: 'Invalid email or password' });
+            }
+            
+            console.log('User with ID: ', user._id, ' and Email of: ', user.email, ' has logged in successfully!')
+
+            return res.status(200).json({ user });
+        } catch (error) {
+            console.log('Login Error: ', error)
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    } else {
+        return res.status(405).json({ error: 'Method Not Allowed' });
     }
 };
-
-const handler = NextAuth(authOptions);
-
-export {handler as GET, handler as POST};
